01102ae149c0438a1c2fae101edeadf7481d1aad
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-11-18 19:31:45 +0800

    add logout handler

--- src/components/Dashboard/DashboardHeader.tsx
+++ src/components/Dashboard/DashboardHeader.tsx
@@ -2,12 +2,19 @@
 import LogoutIcon from "../../images/icons/log-out.svg?react";
 
 // components import
-import { LinkButtonSecondary } from "../Button";
+import { ButtonTertiary } from "../Button";
 import ThemeToggle from "../ThemeToggle";
 import Icon from "../Icon";
 import { logout } from "../../actions/logout";
+import { redirect } from "react-router";
 
 function DashboardHeader() {
+  const handleLogout = () => {
+    logout().then(() => {
+      redirect("/login/");
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 col-span-3 col-start-1 flex h-fit flex-row flex-wrap items-center justify-between border-b border-b-accent/10 bg-background px-4 py-3 text-center sm:px-8 md:px-12 lg:px-24 xl:px-40">
       <p className="text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl">
@@ -18,10 +25,10 @@ function DashboardHeader() {
         <ThemeToggle />
 
         <div className="flex flex-row items-center gap-2">
-          <LinkButtonSecondary onClick={() => logout()} to="/login/">
+          <ButtonTertiary onClick={handleLogout}>
             <Icon SvgIcon={LogoutIcon} isBorderless />
-            <span className="hidden md:block">Log Out</span>
-          </LinkButtonSecondary>
+            <span className="hidden md:block md:text-nowrap">Log Out</span>
+          </ButtonTertiary>
         </div>
       </div>
     </header>
