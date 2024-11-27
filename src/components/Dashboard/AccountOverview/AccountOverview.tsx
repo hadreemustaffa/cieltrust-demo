c69b20f3cb8646265ce6400532398fa39647be2d
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-11-27 15:58:53 +0800

    style: change grid style

--- src/components/Dashboard/AccountOverview/AccountOverview.tsx
+++ src/components/Dashboard/AccountOverview/AccountOverview.tsx
@@ -1,6 +1,6 @@
 function AccountOverview({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-auto-fit-sm gap-4 rounded-md border border-accent/10 p-4 text-left">
+    <div className="grid grid-cols-1 gap-4 rounded-md border border-accent/10 p-4 text-left md:grid-cols-2 2xl:grid-cols-4">
       {children}
     </div>
   );
