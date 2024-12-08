5cb972c8cdc1df676006a9807d47cc60a657d2e4
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-12-09 02:53:18 +0800

    remove useMemo and clean up file
    - using useMemo at this stage is a bit
    premature optimization

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -1,15 +1,14 @@
 import { useLoaderData } from "react-router";
-import AccountOverview, {
-  Overview,
-} from "../../components/Dashboard/AccountOverview/AccountOverview";
-import { useSession } from "../../context/SessionContext";
 import supabase from "../../utils/supabase";
-import { deleteUser } from "../../actions/delete";
+
+// components import
 import SavingGoals, {
   SavingGoalFormProps,
 } from "../../components/Dashboard/SavingGoals/SavingGoals";
 import Budget, { BudgetTable } from "../../components/Dashboard/Budget/Budget";
-import { useMemo } from "react";
+import AccountOverview, {
+  Overview,
+} from "../../components/Dashboard/AccountOverview/AccountOverview";
 
 interface DashboardProps {
   id: number;
@@ -33,7 +32,6 @@ export async function dashboardLoader() {
   }
 
   if (data) {
-    console.log(data);
     return data;
   }
 }
@@ -41,42 +39,14 @@ export async function dashboardLoader() {
 export default function Dashboard() {
   let data = useLoaderData() as DashboardProps;
 
-  console.log(data);
-
-  const { session } = useSession();
-
-  const accountOverviewProps = useMemo(
-    () => ({
-      dashboardId: data.id,
-      data: data.overview,
-    }),
-    [data.id, data.overview],
-  );
-
-  const savingGoalsProps = useMemo(
-    () => ({
-      dashboardId: data.id,
-      data: data.saving_goals,
-    }),
-    [data.id, data.saving_goals],
-  );
-
-  const budgetProps = useMemo(
-    () => ({
-      dashboardId: data.id,
-      data: data.budget,
-    }),
-    [data.id, data.budget],
-  );
-
   return (
     <div className="flex flex-col gap-4 text-left">
-      <AccountOverview {...accountOverviewProps} />
+      <AccountOverview dashboardId={data.id} data={data.overview} />
 
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         <div className="col-span-2 flex flex-col gap-4">
-          <SavingGoals {...savingGoalsProps} />
-          <Budget {...budgetProps} />
+          <SavingGoals dashboardId={data.id} data={data.saving_goals} />
+          <Budget dashboardId={data.id} data={data.budget} />
         </div>
       </div>
     </div>
