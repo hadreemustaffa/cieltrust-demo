79a43bf6c0c151e74c60e694390bd2d2c04d5405
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-06 17:30:31 +0800

    feat: add proper display for zero value

--- src/routes/dashboard/upcoming-payment/upcoming-payment.tsx
+++ src/routes/dashboard/upcoming-payment/upcoming-payment.tsx
@@ -56,7 +56,7 @@ export default function UpcomingPayment() {
         {budgetTables.length > 0 ? (
           <>
             <div className="flex flex-col items-center justify-center rounded-md border border-accent/10 bg-card p-4 text-center">
-              <p className="text-lg font-medium">${totalExpectedPayments}</p>
+              <p className="text-lg font-medium">{totalExpectedPayments < 0 ? '$0.00' : `$${totalExpectedPayments}`}</p>
               <p className="text-sm text-copy/70">Estimated payments for the upcoming month</p>
             </div>
 
@@ -76,10 +76,10 @@ export default function UpcomingPayment() {
                     {budget.budget_categories.map((category) => (
                       <div
                         key={category.id}
-                        className={`flex flex-row items-center justify-between gap-2 ${category.amount - category.spent === 0 ? 'text-copy/50' : ''}`}
+                        className={`flex flex-row items-center justify-between gap-2 ${category.amount - category.spent <= 0 ? 'text-copy/50' : ''}`}
                       >
                         <p>{category.name}</p>
-                        <p>${category.amount - category.spent}</p>
+                        <p>{category.amount - category.spent <= 0 ? '0' : category.amount - category.spent}</p>
                       </div>
                     ))}
                   </li>
