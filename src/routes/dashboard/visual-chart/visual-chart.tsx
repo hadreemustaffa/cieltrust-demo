c20c9db1753a9a492b733e18268a5d547f00126f
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-06 17:02:46 +0800

    feat: improve chart interface

--- src/routes/dashboard/visual-chart/visual-chart.tsx
+++ src/routes/dashboard/visual-chart/visual-chart.tsx
@@ -21,11 +21,12 @@ dayjs.extend(isBetween);
 
 ChartJS.register(CategoryScale, Filler, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-type TimePeriod = 'month' | '6 months' | 'year' | 'year-to-date';
-
 export default function VisualChart({ data }: VisualChartProps) {
-  const [selectedPeriod, setSelectedPeriod] = useState<TimePeriod>('month');
+  const [selectedPeriod, setSelectedPeriod] = useState<TimePeriod>('1M');
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(dayjs());
+
+  const PERIODS = ['1M', '6M', '1Y', 'YTD'] as const;
+  type TimePeriod = (typeof PERIODS)[number];
 
   const availableMonths = useMemo(() => {
     return Array.from(new Set(data.map((transaction) => dayjs(transaction.transaction_date).format('YYYY-MM')))).sort();
@@ -37,24 +38,24 @@ export default function VisualChart({ data }: VisualChartProps) {
     let labels: string[];
 
     switch (period) {
-      case 'month':
+      case '1M':
         startDate = selectedDate.startOf('month');
         endDate = selectedDate.endOf('month');
         labels = Array.from({ length: dayjs(endDate).diff(startDate, 'day') + 1 }, (_, i) =>
           startDate.add(i, 'day').format('YYYY-MM-DD'),
         );
         break;
-      case '6 months':
+      case '6M':
         startDate = selectedDate.subtract(5, 'month').startOf('month');
         endDate = selectedDate.endOf('month');
         labels = Array.from({ length: 6 }, (_, i) => startDate.add(i, 'month').format('YYYY-MM'));
         break;
-      case 'year':
+      case '1Y':
         startDate = selectedDate.subtract(1, 'year').startOf('month');
         endDate = selectedDate.endOf('month');
         labels = Array.from({ length: 12 }, (_, i) => startDate.add(i, 'month').format('YYYY-MM'));
         break;
-      case 'year-to-date':
+      case 'YTD':
         startDate = selectedDate.startOf('year');
         endDate = selectedDate;
         labels = Array.from({ length: dayjs(endDate).diff(startDate, 'month') + 1 }, (_, i) =>
@@ -64,9 +65,9 @@ export default function VisualChart({ data }: VisualChartProps) {
     }
 
     const expensesData = labels.map((label) => {
-      const periodStart = period === 'month' ? dayjs(label) : dayjs(label + '-01');
+      const periodStart = period === '1M' ? dayjs(label) : dayjs(label + '-01');
 
-      const periodEnd = period === 'month' ? dayjs(label) : periodStart.endOf('month');
+      const periodEnd = period === '1M' ? dayjs(label) : periodStart.endOf('month');
 
       return data
         .filter(
@@ -78,9 +79,9 @@ export default function VisualChart({ data }: VisualChartProps) {
     });
 
     const incomeData = labels.map((label) => {
-      const periodStart = period === 'month' ? dayjs(label) : dayjs(label + '-01');
+      const periodStart = period === '1M' ? dayjs(label) : dayjs(label + '-01');
 
-      const periodEnd = period === 'month' ? dayjs(label) : periodStart.endOf('month');
+      const periodEnd = period === '1M' ? dayjs(label) : periodStart.endOf('month');
 
       return data
         .filter(
@@ -108,11 +109,15 @@ export default function VisualChart({ data }: VisualChartProps) {
     scales: {
       y: {
         beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Amount',
+        },
       },
       x: {
         title: {
           display: true,
-          text: `Transactions (${selectedPeriod})`,
+          text: `Time Period (${selectedPeriod})`,
         },
       },
     },
@@ -122,12 +127,12 @@ export default function VisualChart({ data }: VisualChartProps) {
 
   const chartData = {
     labels:
-      selectedPeriod === 'month'
+      selectedPeriod === '1M'
         ? labels
         : labels.map((label) =>
-            selectedPeriod === '6 months'
+            selectedPeriod === '6M'
               ? dayjs(label + '-01').format('MMM YYYY')
-              : selectedPeriod === 'year-to-date'
+              : selectedPeriod === 'YTD'
                 ? dayjs(label + '-01').format('MMM')
                 : dayjs(label + '-01').format('MMM YYYY'),
           ),
@@ -154,32 +159,34 @@ export default function VisualChart({ data }: VisualChartProps) {
       <div className="flex w-full flex-col gap-4 overflow-x-scroll md:overflow-x-auto">
         <div className="flex flex-row flex-wrap items-center gap-2">
           <div className="flex flex-row flex-wrap items-center gap-2">
-            {(['month', '6 months', 'year', 'year-to-date'] as TimePeriod[]).map((period) => (
+            {PERIODS.map((period) => (
               <ButtonSecondary
                 key={period}
                 className={`${selectedPeriod === period ? 'bg-accent/10' : ''}`}
                 onClick={() => setSelectedPeriod(period)}
               >
-                {period === 'month' ? '1M' : period === '6 months' ? '6M' : period === 'year' ? '1Y' : 'YTD'}
+                {period === '1M' ? '1M' : period === '6M' ? '6M' : period === '1Y' ? '1Y' : 'YTD'}
               </ButtonSecondary>
             ))}
           </div>
 
-          <select
-            id="selectMonth"
-            value={selectedDate.format('YYYY-MM')}
-            onChange={(e) => {
-              const newDate = dayjs(e.target.value + '-01');
-              setSelectedDate(newDate);
-            }}
-            className="rounded border bg-card p-2"
-          >
-            {availableMonths.map((month) => (
-              <option key={month} value={month}>
-                {dayjs(month + '-01').format('MMMM YYYY')}
-              </option>
-            ))}
-          </select>
+          {availableMonths.length !== 0 && (
+            <select
+              id="selectMonth"
+              value={selectedDate.format('YYYY-MM')}
+              onChange={(e) => {
+                const newDate = dayjs(e.target.value + '-01');
+                setSelectedDate(newDate);
+              }}
+              className="rounded border bg-card p-2"
+            >
+              {availableMonths.map((month) => (
+                <option key={month} value={month}>
+                  {dayjs(month + '-01').format('MMMM YYYY')}
+                </option>
+              ))}
+            </select>
+          )}
         </div>
 
         <Line data={chartData} options={options} />
