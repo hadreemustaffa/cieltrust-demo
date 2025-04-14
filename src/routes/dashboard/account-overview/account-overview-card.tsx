8cf2f8de9a72554cab84a52d718b91a2a355cd29
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-14 12:59:54 +0800

    feat: add tooltip to income and expenses

--- src/routes/dashboard/account-overview/account-overview-card.tsx
+++ src/routes/dashboard/account-overview/account-overview-card.tsx
@@ -1,10 +1,16 @@
 import Icon from '@/components/icon';
+import Tooltip from '@/components/tooltip';
 import CreditCardIcon from '@/images/icons/credit-card.svg?react';
 import TrendingDownIcon from '@/images/icons/trending-down.svg?react';
 import TrendingUpIcon from '@/images/icons/trending-up.svg?react';
 import { AccountOverviewCardProps } from '@/routes/dashboard/account-overview/account-overview.types';
 
-export default function AccountOverviewCard({ amount, lastMonthAmount, columnTitle }: AccountOverviewCardProps) {
+export default function AccountOverviewCard({
+  amount,
+  lastMonthAmount,
+  columnTitle,
+  hasTooltip,
+}: AccountOverviewCardProps) {
   const percentDifference = ((amount - lastMonthAmount) / lastMonthAmount) * 100;
 
   return (
@@ -12,9 +18,10 @@ export default function AccountOverviewCard({ amount, lastMonthAmount, columnTit
       <div className="flex flex-col gap-2 rounded-md border border-accent/10 bg-surface p-4">
         <div className="flex flex-col justify-between gap-2 text-left">
           <div className="flex flex-row items-center justify-between">
-            <div className="flex flex-row items-center gap-2">
+            <div className="relative flex flex-row items-center gap-2">
               <Icon SvgIcon={CreditCardIcon} width={16} height={16} isBorderless />
               <p className="text-sm">{columnTitle[0].toUpperCase() + columnTitle.substring(1)}</p>
+              {hasTooltip && <Tooltip text="This value resets on the first day of each month" />}
             </div>
             {lastMonthAmount <= amount ? (
               <div className="flex flex-row items-center gap-2 rounded-sm bg-green-800 px-2 py-1 text-xs text-white">
