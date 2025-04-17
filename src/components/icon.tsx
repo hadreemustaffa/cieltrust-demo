98f4d2de94a155ee3a14d859a18318e3939ecdb9
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-17 09:18:32 +0800

    add cn utility to component

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-type IconProps = {
+import { cn } from '@/utils/cn';
+
+interface IconProps extends React.HTMLAttributes<HTMLSpanElement> {
   SvgIcon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   isBorderless?: boolean;
   width?: number;
   height?: number;
-};
+}
 
-export default function Icon({ SvgIcon, isBorderless, width = 24, height = 24, ...props }: IconProps) {
+export default function Icon({ SvgIcon, isBorderless, width = 24, height = 24, className, ...props }: IconProps) {
   return (
     <span
       aria-hidden="true"
-      className={`flex rounded-md ${isBorderless ? '' : 'border border-accent/10 p-2'}`}
+      className={cn(`flex rounded-md ${isBorderless ? '' : 'border border-accent/10 p-2'}`, className)}
       {...props}
     >
       <SvgIcon width={width} height={height} />
