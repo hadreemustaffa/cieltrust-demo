49d4a7af97d00364e7d78d2634f07bec43b4ab75
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-27 07:43:36 +0800

    style: revamp login page for cleaner look

--- src/routes/login/login.tsx
+++ src/routes/login/login.tsx
@@ -1,19 +1,16 @@
-import Testimonials from '@/components/testimonials';
 import LoginForm from '@/routes/login/login-form';
 
 export default function Login() {
   return (
-    <>
-      <div className="flex flex-col items-center gap-8 rounded-md border border-accent/10 p-8">
-        <div className="flex max-w-4xl flex-col gap-4">
-          <h1 className="text-3xl font-medium lg:text-5xl">Login</h1>
-          <p>Welcome back! Please log in to access your account</p>
+    <div className="flex h-[80vh] flex-col items-center justify-center gap-8 rounded-md border border-accent/10 p-4">
+      <div className="flex w-full max-w-96 flex-col gap-4 md:w-96">
+        <div className="flex flex-col gap-2 pb-4">
+          <h1 className="text-3xl font-medium">Welcome back</h1>
+          <p>Log in to your account</p>
         </div>
 
         <LoginForm />
       </div>
-
-      <Testimonials />
-    </>
+    </div>
   );
 }
