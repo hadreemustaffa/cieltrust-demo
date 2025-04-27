80ccfac2dbe26a5ee379c9a1347dd9e4a9dacdff
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-04-27 18:17:31 +0800

    style: revamp sign up page looks
    - remove disclaimer from the page
    - add prop to  disclaimer for future use

--- src/routes/sign-up/sign-up-form.tsx
+++ src/routes/sign-up/sign-up-form.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import { signup } from '@/actions/signup';
 import { ButtonPrimary } from '@/components/button';
 import ErrorMessage from '@/components/error-message';
+import { Input } from '@/components/forms/custom-form';
+import { PasswordInput } from '@/components/password';
 import { EMAIL_REGEX } from '@/data/constants';
 import { ERROR_MSG } from '@/data/errorMessages';
-import EyeOffIcon from '@/images/icons/eye-off.svg?react';
-import EyeIcon from '@/images/icons/eye.svg?react';
 
 interface SignUpFormData {
   id: string;
@@ -19,22 +19,21 @@ interface SignUpFormData {
 }
 
 export default function SignUpForm() {
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
+
+  const methods = useForm<SignUpFormData>();
 
   const {
     register,
     handleSubmit,
     getValues,
-    formState: { errors },
-  } = useForm<SignUpFormData>();
+    formState: { errors, isSubmitting },
+  } = methods;
 
   const onSubmit: SubmitHandler<SignUpFormData> = async () => {
-    setIsLoading(true);
     setIsError(false);
     setErrorMessage('');
 
@@ -46,8 +45,6 @@ export default function SignUpForm() {
         password: getValues('password'),
       });
 
-      setIsLoading(false);
-
       if (result?.error) {
         setIsError(true);
         setErrorMessage(result.error.message);
@@ -55,7 +52,6 @@ export default function SignUpForm() {
         navigate('/dashboard/');
       }
     } catch (error) {
-      setIsLoading(false);
       setIsError(true);
       setErrorMessage('An unexpected error occurred');
       console.log(error);
@@ -63,117 +59,77 @@ export default function SignUpForm() {
   };
 
   return (
-    <form
-      className="flex w-full max-w-4xl flex-col items-center gap-4 lg:items-start"
-      autoComplete="off"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex w-full flex-col gap-4 md:flex-row">
-        <div className="flex w-full flex-col gap-2 text-left">
-          <label htmlFor="firstName" className="text-sm">
-            First Name:
-          </label>
-          <input
-            id="firstName"
-            type="text"
-            className="w-full rounded-md border border-accent/10 bg-transparent px-4 py-2"
-            autoComplete="given-name"
-            aria-invalid={errors.firstName ? 'true' : 'false'}
-            disabled={isLoading}
-            {...register('firstName', {
-              required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
-              maxLength: { value: 30, message: ERROR_MSG.MAX_LENGTH_EXCEEDED },
-            })}
-          />
-          {errors.firstName && <ErrorMessage error={errors.firstName.message} />}
-        </div>
-
-        <div className="flex w-full flex-col gap-2 text-left">
-          <label htmlFor="lastName" className="text-sm">
-            Last Name:
-          </label>
-          <input
-            id="lastName"
-            type="text"
-            className="w-full rounded-md border border-accent/10 bg-transparent px-4 py-2"
-            autoComplete="family-name"
-            aria-invalid={errors.lastName ? 'true' : 'false'}
-            disabled={isLoading}
-            {...register('lastName', {
-              required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
-              maxLength: { value: 30, message: ERROR_MSG.MAX_LENGTH_EXCEEDED },
-            })}
-          />
-          {errors.lastName && <ErrorMessage error={errors.lastName.message} />}
-        </div>
-      </div>
-
-      <div className="flex w-full flex-col gap-4 md:flex-row">
-        <div className="flex w-full flex-col gap-2 text-left">
-          <label htmlFor="email" className="text-sm">
-            Email:
-          </label>
-          <input
-            id="email"
-            className="w-full rounded-md border border-accent/10 bg-transparent px-4 py-2"
-            autoComplete="email"
-            aria-invalid={errors.email ? 'true' : 'false'}
-            disabled={isLoading}
-            {...register('email', {
-              required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
-              pattern: { value: EMAIL_REGEX, message: ERROR_MSG.INVALID_EMAIL },
-            })}
-          />
-          {errors.email && <ErrorMessage error={errors.email.message} />}
-        </div>
-
-        <div className="flex w-full flex-col gap-2">
+    <FormProvider {...methods}>
+      <form
+        className="flex w-full flex-col items-center gap-8 lg:items-start"
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="flex w-full flex-col gap-4">
           <div className="flex w-full flex-col gap-2 text-left">
-            <label className="text-sm" htmlFor="password">
-              Password:
+            <label htmlFor="firstName" className="text-sm">
+              First Name:
             </label>
-            <div className="relative">
-              <input
-                type={isPasswordVisible ? 'text' : 'password'}
-                id="password"
-                className="w-full rounded-md border border-accent/10 bg-transparent px-4 py-2"
-                autoComplete="new-password"
-                aria-invalid={errors.password ? 'true' : 'false'}
-                disabled={isLoading}
-                {...register('password', {
-                  required: {
-                    value: true,
-                    message: ERROR_MSG.FIELD_IS_REQUIRED,
-                  },
-                  minLength: {
-                    value: 8,
-                    message: ERROR_MSG.PASSWORD_TOO_SHORT,
-                  },
-                })}
-              />
-              <button
-                type="button"
-                className="absolute right-4 top-1/2 -translate-y-1/2 rounded-md text-sm text-accent/20 transition-colors duration-300 hover:text-accent-hover"
-                aria-controls="password"
-                aria-label="toggle password visibility"
-                onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                disabled={isLoading}
-              >
-                {isPasswordVisible ? <EyeIcon /> : <EyeOffIcon />}
-              </button>
-            </div>
-            {errors.password && <ErrorMessage error={errors.password.message} />}
+            <Input
+              id="firstName"
+              type="text"
+              autoComplete="given-name"
+              aria-invalid={errors.firstName ? 'true' : 'false'}
+              {...register('firstName', {
+                required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
+                maxLength: { value: 30, message: ERROR_MSG.MAX_LENGTH_EXCEEDED },
+              })}
+            />
+            {errors.firstName && <ErrorMessage error={errors.firstName.message} />}
           </div>
+
+          <div className="flex w-full flex-col gap-2 text-left">
+            <label htmlFor="lastName" className="text-sm">
+              Last Name:
+            </label>
+            <Input
+              id="lastName"
+              type="text"
+              autoComplete="family-name"
+              aria-invalid={errors.lastName ? 'true' : 'false'}
+              {...register('lastName', {
+                required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
+                maxLength: { value: 30, message: ERROR_MSG.MAX_LENGTH_EXCEEDED },
+              })}
+            />
+            {errors.lastName && <ErrorMessage error={errors.lastName.message} />}
+          </div>
+
+          <div className="flex w-full flex-col gap-2 text-left">
+            <label htmlFor="email" className="text-sm">
+              Email:
+            </label>
+            <Input
+              id="email"
+              type="email"
+              autoComplete="off"
+              aria-invalid={errors.email ? 'true' : 'false'}
+              {...register('email', {
+                required: { value: true, message: ERROR_MSG.FIELD_IS_REQUIRED },
+                pattern: { value: EMAIL_REGEX, message: ERROR_MSG.INVALID_EMAIL },
+              })}
+            />
+            {errors.email && <ErrorMessage error={errors.email.message} />}
+          </div>
+
+          <PasswordInput id="password" label="Password" isRequired />
+
+          {isError && <ErrorMessage error={errorMessage} />}
         </div>
-      </div>
 
-      {isError && <ErrorMessage error={errorMessage || ERROR_MSG.INVALID_CREDENTIALS} />}
-
-      <div className="flex w-full justify-end">
-        <ButtonPrimary type="submit" disabled={isLoading}>
-          {isLoading ? 'Signing up...' : 'Sign up'}
+        <ButtonPrimary
+          type="submit"
+          className={`w-full ${isSubmitting ? 'animate-pulse bg-brand-hover' : ''} `}
+          disabled={isSubmitting}
+        >
+          Sign Up
         </ButtonPrimary>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 }
