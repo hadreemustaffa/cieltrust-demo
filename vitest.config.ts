4ce222e8bde5f6856a49be63442994f679e8b14c
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-05-14 16:55:07 +0800

    add package and update config for future testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
-  },
-});
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import viteConfig from './vite.config';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.ts',
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }),
+);
