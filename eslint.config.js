15b4c7f3dcb2274bb9bc632e4b2168a836816bae
hadreemustaffa <hadree.mustaffa@gmail.com>
2025-05-29 20:21:56 +0800

    chore(eslint): no-unused-vars now warn instead of error

--- eslint.config.js
+++ eslint.config.js
@@ -55,6 +55,14 @@ export default [
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn', // or "error"
+        {
+          argsIgnorePattern: '_',
+          varsIgnorePattern: '_',
+          caughtErrorsIgnorePattern: '_',
+        },
+      ],
       'import/order': [
         'error',
         {
